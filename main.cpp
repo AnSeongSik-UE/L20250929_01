ca99fbbf81ff3732415c704ce396cd432153632d
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-29 17:04:04 +0900

    더블 버퍼링
    더블 버퍼링

--- main.cpp
+++ main.cpp
@@ -4,29 +4,43 @@
 
 using namespace std;
 
+enum ERenderScreenBuffer
+{
+	FrontBuffer = 0,
+	BackBuffer = 1
+};
+
+int CurrentBufferIndex = FrontBuffer;
+
 struct FCharacter
 {
 	int X;
 	int Y;
-	char Shape;
+	string Shape;
 };
 
 FCharacter Characters[2];
 
 int KeyCode;
 bool bIsPlay;
+HANDLE FrontBufferHandle;
+HANDLE BackBufferHandle;
 
 void Init()
 {
+	// 0, nullptr, NULL 전부 0
+	FrontBufferHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, nullptr, CONSOLE_TEXTMODE_BUFFER, NULL);
+	BackBufferHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, nullptr, CONSOLE_TEXTMODE_BUFFER, NULL);
+
 	//형변환, Casting
 	srand((unsigned int)time(nullptr));
 	Characters[0].X = 1;
 	Characters[0].Y = 1;
-	Characters[0].Shape = 'P';
+	Characters[0].Shape = "P";
 
 	Characters[1].X = 10;
 	Characters[1].Y = 10;
-	Characters[1].Shape = 'M';
+	Characters[1].Shape = "M";
 
 	bIsPlay = true;
 }
@@ -102,23 +116,83 @@ void Tick()
 	MoveMonster();
 }
 
-void RenderCharacter(FCharacter InData)
+void Present()
+{
+	if (CurrentBufferIndex == FrontBuffer)
+	{
+		SetConsoleActiveScreenBuffer(FrontBufferHandle);
+	}
+	else
+	{
+		SetConsoleActiveScreenBuffer(BackBufferHandle);
+	}
+
+	++CurrentBufferIndex;
+	CurrentBufferIndex = CurrentBufferIndex % 2;
+}
+
+//C++
+void RenderCharacter(const FCharacter* InData)
 {
 	COORD Position;
-	Position.X = (SHORT)InData.X;
-	Position.Y = (SHORT)InData.Y;
+	Position.X = (SHORT)InData->X;
+	Position.Y = (SHORT)InData->Y;
+	//Position.Y = (SHORT)(*(InData).Y);
+	
+	if (CurrentBufferIndex == FrontBuffer)
+	{
+		SetConsoleCursorPosition(FrontBufferHandle, Position);
+		WriteConsole(FrontBufferHandle, InData->Shape.c_str(), 1, NULL, NULL);
+	}
+	else
+	{
+		SetConsoleCursorPosition(BackBufferHandle, Position);
+		WriteConsole(BackBufferHandle, InData->Shape.c_str(), 1, NULL, NULL);
+	}
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Position);
-	cout << InData.Shape;
+}
+
+void Clear()
+{
+	COORD coordScreen = { 0, 0 };    // home for the cursor
+	DWORD cCharsWritten;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD dwConSize;
+
+	if (CurrentBufferIndex == FrontBuffer)
+	{
+		//스크린 버퍼 정보 가져오기
+		GetConsoleScreenBufferInfo(FrontBufferHandle, &csbi);
+		dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+		FillConsoleOutputCharacter(FrontBufferHandle,
+			(TCHAR)' ',
+			dwConSize,
+			coordScreen,
+			&cCharsWritten);
+	}
+	else
+	{
+		GetConsoleScreenBufferInfo(BackBufferHandle, &csbi);
+		dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+		FillConsoleOutputCharacter(BackBufferHandle,
+			(TCHAR)' ',
+			dwConSize,
+			coordScreen,
+			&cCharsWritten);
+	}
 }
 
 void Render()
 {
-	system("cls");
+	Clear();
+	
+	//Render All
 	for (int i = 0; i < 2; i++)
 	{
-		RenderCharacter(Characters[i]);
+		RenderCharacter(&Characters[i]);
 	}
+
+	Present();
 }
 
 int main()
